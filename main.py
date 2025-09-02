052d11be7b3e2fef89d569cf8e4886648faa8558
berchtold@hg-epp.de <berchtold@hg-epp.de>
2025-09-02 07:18:28 +0200

    2nd part of video done: Movement almost finished

--- main.py
+++ main.py
@@ -113,42 +113,61 @@ def get_random_pos(tiles):
 
 def move_tiles(window, tiles, clock, direction):
     updated = True
-    # which blocks have already been merged
+    # blocks that have already been merged, will be saved in blocks
     blocks = set()
 
     if direction == "left":
         sort_func = lambda x: x.col
-        reverse = False
+        rev = False
         delta = (-MOVE_VEL, 0)
         boundary_check = lambda tile: tile.col == 0
         get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+        # should the tiles be merged, based on the current movement of the tile?
         merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_VEL
+        # the movement should stop, when the border of the next_tile is reached
         move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_VEL
+        # round up or round down at the location of the tile
         ceil = True
     elif direction == "right":
-
+        sort_func = lambda x: x.col
+        rev = True
+        delta = (MOVE_VEL, 0)
+        boundary_check = lambda tile: tile.col == COLS - 1
+        get_next_title = lambda title: tiles.get(f"{tile.row}{tile.col + 1}")
+        merge_check = lambda tile, next_tile: tile.x < next_tile.x - MOVE_VEL
+        move_check = lambda tile, next_tile: tile.x < next_tile.x - RECT_WIDTH - MOVE_VEL
+        ceil = True
     elif direction == "up":
+        sort_func = lambda y: y.row
+        rev = False
+        delta = (0, -MOVE_VEL)
+        boundary_check = lambda tile: 
         
     elif direction == "down":
 
     while updated: 
         clock.tick(FPS)
         updated = False
-        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse = reverse)
+        sorted_tiles = sorted(tiles.values(), key=sort_func, reverse = rev)
 
         for i, tile in enumerate(sorted_tiles):
+            # if tile reached the boundary, there is nothing left to do
             if boundary_check(tile):
                 continue
 
             next_tile = get_next_tile(tile)
+            # if there is no tile in the way, move the selected tile
             if not next_tile:
                 tile.move(delta)
             elif tile.value == next_tile.value and tile not in blocks and next_tile in blocks:
+                # if we're in the process of merging, then move the tile
                 if merge_check(tile, next_tile):
                     tile.move(delta)
+                    # else performe the merge operation
                 else:
                     next_tile.value *= 2
                     sorted_tiles.pop(i)
+                    # only next_tile has to be added to blocks. The selected tile was poped out of sorted_tiles
                     blocks.add(next_tile)
 
 def generate_tiles():
